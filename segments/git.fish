7b269b5d1ef160da07ee3b133b3ad6be703c0fa3
0rax <roemer.jp@gmail.com>
2014-09-02 17:57:59 +0200

    Git

--- segments/git.fish
+++ segments/git.fish
@@ -3,19 +3,18 @@
 
 function FLINE_GIT
 
-	git status ^^ /dev/null > .gitstatus
+	git status ^^ /dev/null > /tmp/gitstatus
 	if test $status -eq 0
-		set -l branch (cat .gitstatus | grep "On branch" | cut -d" " -f3-)
-		set -l staged (cat .gitstatus | grep "Changes to be committed")
-		set -l unstaged (cat .gitstatus | grep "Changes not staged for commit")
-		set -l untracked (cat .gitstatus | grep "Untracked files")
+		set -l branch (cat /tmp/gitstatus | grep "On branch" | cut -d" " -f3-)
+		set -l staged (cat /tmp/gitstatus | grep "Changes to be committed")
+		set -l unstaged (cat /tmp/gitstatus | grep "Changes not staged for commit")
+		set -l untracked (cat /tmp/gitstatus | grep "Untracked files")
 		__close_prev $FLINE_BG_GIT $FLINE_FG_GIT
 		echo -n $FLINE_GIT_BRANCH
 		echo -n "$branch "
-		printf $untracked
-		if test -n $untracked
+		if test -n "$untracked"
 			echo -n "+ "
 		end
 	end
-	rm -f .gitstatus
+	rm -f /tmp/gitstatus
 end

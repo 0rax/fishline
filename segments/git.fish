2f6caba0abcfe0ca011e9a14494cc480f4207d74
0rax <roemer.jp@gmail.com>
2015-01-12 20:37:09 +0100

    use of read instead of a tmp file for GIT Segment

--- segments/git.fish
+++ segments/git.fish
@@ -3,22 +3,31 @@
 
 function FLSEG_GIT
 
-	git status ^^ /dev/null > /tmp/gitstatus
+	set -l gitstatus ""
+	git status ^^ /dev/null | \
+	while read line
+		set gitstatus $gitstatus\n$line
+	end
+
 	if test $status -eq 0
-		set -l branch (cat /tmp/gitstatus | grep "On branch" | cut -d" " -f3-)
-		set -l staged (cat /tmp/gitstatus | grep "Changes to be committed")
-		set -l unstaged (cat /tmp/gitstatus | grep "Changes not staged for commit")
-		set -l untracked (cat /tmp/gitstatus | grep "Untracked files")
-		set -l ahead (cat /tmp/gitstatus | grep "Your branch is ahead" | grep -o '[0-9]\+ commit' | cut -d" " -f1)
-		set -l behind (cat /tmp/gitstatus | grep "Your branch is behind" | grep -o '[0-9]\+ commit' | cut -d" " -f1)
+		
+		set -l branch    (echo $gitstatus | grep "On branch" | cut -d" " -f3-)
+		set -l staged    (echo $gitstatus | grep "Changes to be committed")
+		set -l unstaged  (echo $gitstatus | grep "Changes not staged for commit")
+		set -l untracked (echo $gitstatus | grep "Untracked files")
+		set -l ahead     (echo $gitstatus | grep "Your branch is ahead" | grep -o '[0-9]\+ commit' | cut -d" " -f1)
+		set -l behind    (echo $gitstatus | grep "Your branch is behind" | grep -o '[0-9]\+ commit' | cut -d" " -f1)
+
 		if test -n "$staged" -o -n "$unstaged" -o -n "$untracked"
 		   set dirty yes
 		end
+		
 		if set -q dirty
 		   FLINT_CLOSE $FLCLR_GIT_BG_DIRTY $FLCLR_GIT_FG_DIRTY
 		else
 		   FLINT_CLOSE $FLCLR_GIT_BG_CLEAN $FLCLR_GIT_FG_CLEAN
 		end
+		
 		echo -n $FLSYM_GIT_BRANCH
 		echo -n "$branch "
 		if test -n "$ahead"
@@ -30,7 +39,7 @@ function FLSEG_GIT
 		if test -n "$untracked"
 			echo -n "$FLSYM_GIT_UNTRACKED"
 		end
+		
 	end
-	rm -f /tmp/gitstatus
 
 end

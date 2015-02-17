57cd42e61dff4763f62531ee7c7415e7f2fed482
Carmine Paolino <carmine@paolino.me>
2015-02-17 19:50:28 +0100

    Fix VFISH segment printing a newline
    plus it’s cleaner ;)

--- segments/vfish.fish
+++ segments/vfish.fish
@@ -6,7 +6,7 @@ function FLSEG_VFISH
 
 	if set -q VIRTUAL_ENV
 		FLINT_CLOSE $FLCLR_VFISH_BG $FLCLR_VFISH_FG
-		printf $FLSYM_VFISH" "$VIRTUAL_ENV  | sed 's/\/.*\///; s/^/ /; s/$/ /'
+		printf $FLSYM_VFISH" "(basename $VIRTUAL_ENV)
 	end
 
 end

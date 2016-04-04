f7186aba95064ee814df819c97966a581f9dff6b
Jean-Philippe Roemer <jp@roemer.im>
2016-04-04 01:45:40 +0100

    Added test for EXECTIME segments

--- segments/exectime.fish
+++ segments/exectime.fish
@@ -6,6 +6,21 @@ set FLCLR_EXECTIME_FG black
 
 function FLTEST_EXECTIME
 
+  echo "Context: Last command took 42ms to complete"
+  set -gx CMD_DURATION 42
+  FLINT_TEST EXECTIME
+
+  echo "Context: Last command took 360ms to complete"
+  set -gx CMD_DURATION 360
+  FLINT_TEST EXECTIME
+
+  echo "Context: Last command took 1042ms to complete"
+  set -gx CMD_DURATION 21042
+  FLINT_TEST EXECTIME
+
+  echo "Context: Last command took 162042ms to complete"
+  set -gx CMD_DURATION 162320
+  FLINT_TEST EXECTIME
 
 end
 

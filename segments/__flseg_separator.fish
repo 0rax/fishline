2ebe556172bb779ac2aab942d180ba93bcaa9069
Jean-Philippe Roemer <jp@roemer.im>
2016-06-09 02:24:09 +0200

    segment/separator: cleaner version of separator
    - separator segment now uses the new `__fishline_segment` logic instead
      of a dirty hack

--- segments/__flseg_separator.fish
+++ segments/__flseg_separator.fish
@@ -3,17 +3,7 @@
 
 function __flseg_separator
 
-    if [ "$FLINT_POSITION" = "left" -a "$FLSYM_LEFT_CLOSE" = "" \
-         -o "$FLINT_POSITION" = "right" -a "$FLSYM_RIGHT_OPEN" = "" ]
-        __flseg_space
-    else
-        set -l save_left_post  $FLSYM_LEFT_POST
-        set -l save_right_post $FLSYM_RIGHT_POST
-        set FLSYM_LEFT_POST  ""
-        set FLSYM_RIGHT_POST ""
-        __fishline_segment $FLCLR_SEPARATOR_BG normal
-        set FLSYM_LEFT_POST  $save_left_post
-        set FLSYM_RIGHT_POST $save_right_post
-    end
+    __fishline_segment "$FLCLR_SEPARATOR_BG" "$FLCLR_SEPARATOR_FG" false false
+    printf "$FLSYM_SEPARATOR_SEG"
 
 end

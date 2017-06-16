0rax/fishline

commit 989ce8dc4a9aba3cdbcfc7e6de44bb0ab1e82247

segment/exectime: better handling of time

segments/__flseg_exectime.fish
@@ -10,15 +10,19 @@ function __flseg_exectime
         __fishline_segment $FLCLR_EXECTIME_BG $FLCLR_EXECTIME_FG
 
         set -l ms  (math "$duration%1000")
-        if [ $duration -gt 60000 ]
-          set -l min (math "$duration/60000")
-          set -l s   (math "$duration/1000%60")
-          printf "%sm %ss %sms" $min $s $ms
+        if [ $duration -gt 36000000 ]
+            set -l h   (math "$duration/36000000")
+            set -l min (math "$duration/60000%60")
+            printf "%hh %sm" $h $min
+        else if [ $duration -gt 60000 ]
+            set -l min (math "$duration/60000")
+            set -l s   (math "$duration/1000%60")
+            printf "%sm %ss" $min $s
         else if [ $duration -gt 1000 ]
-          set -l s   (math "$duration/1000%60")
-          printf "%ss %sms" $s $ms
+            set -l s   (math "$duration/1000")
+            printf "%ss %sms" $s $ms
         else
-          printf "%sms" $ms
+            printf "%sms" $ms
         end
 
     end

commit f6e7ada16efb5584624b03f2f18fe5e1e3b8ec62

segment/exectime: fix bug introduced in 989ce8d
- Text was not printed when exectime was supperior to one hour
- Bug was due to a typo in the printf template string

segments/__flseg_exectime.fish
@@ -13,7 +13,7 @@ function __flseg_exectime
         if [ $duration -gt 36000000 ]
             set -l h   (math "$duration/36000000")
             set -l min (math "$duration/60000%60")
-            printf "%hh %sm" $h $min
+            printf "%sh %sm" $h $min
         else if [ $duration -gt 60000 ]
             set -l min (math "$duration/60000")
             set -l s   (math "$duration/1000%60")
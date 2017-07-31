6457be43a9f7a9c236ac9584244ceed3abd21440
Brian McFee <brian.mcfee@nyu.edu>
2017-07-31 14:34:39 -0400

    proper styling of pipes in screen

--- segments/__flseg_screen.fish
+++ segments/__flseg_screen.fish
@@ -5,7 +5,7 @@ function __flseg_screen
 
     if set -q STY
         __fishline_segment $FLCLR_SCREEN_BG $FLCLR_SCREEN_FG
-        printf $FLSYM_SCREEN" "(echo $STY |cut -d'.' -f2-)
+        printf $FLSYM_SCREEN" "(echo $STY | cut -d'.' -f2-)
     end
 
 end

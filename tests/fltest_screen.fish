420e17fbb3a3a6d348b358596e5a55d23258b690
Brian McFee <brian.mcfee@nyu.edu>
2017-07-31 14:32:09 -0400

    fixed tests for screen segment

--- tests/fltest_screen.fish
+++ tests/fltest_screen.fish
@@ -4,12 +4,15 @@
 function fltest_screen
 
     echo "Context: STY var is set to 'fishline_test'"
+    # Test for custom session names
     set -gx STY 76230.test123
-    __fishline_test STY
+    __fishline_test SCREEN
 
-    set -gx 69359.ttys001.Providence
-    __fishline_test STY
+    # Automated OSX-style name
+    set -gx STY 69359.ttys001.Providence
+    __fishline_test SCREEN
 
+    # Automated linux-style name
     set -gx STY 20424.pts-0.userctl
-    __fishline_test STY
+    __fishline_test SCREEN
 end
